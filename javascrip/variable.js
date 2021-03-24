c911a5272ef29c728d1a427e62f2b34e785da2d0
onsped01 <79687157+onsped01@users.noreply.github.com>
2021-03-24 11:57:23 +0700

    Add files via upload

---
+++ javascrip/variable.js
@@ -0,0 +1,10 @@
+
+<button onclick ="myFunction()"> Click Me </button>
+
+<script>
+   function myFunction() {
+      var x = document.getElementById("demo");
+      x.style.color = "red"
+      x.style.font-size = "24px"";
+   }
+</script>
